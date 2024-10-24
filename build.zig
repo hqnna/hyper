6b0db42400d901468f39a2057bc97dd8d3a5f1ae
Hanna Rose <hanna@hanna.lol>
2024-10-24 11:19:59 -0400

    build: use musl as the default abi

--- build.zig
+++ build.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
+const platform = @import("builtin").target.os.tag;
+
+const _target = std.zig.CrossTarget{
+    .abi = if (platform == .linux) .musl else null,
+    .cpu_model = .native,
+    .os_tag = platform,
+    .cpu_arch = null,
+};
 
 pub fn build(b: *std.Build) anyerror!void {
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{ .default_target = _target });
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
